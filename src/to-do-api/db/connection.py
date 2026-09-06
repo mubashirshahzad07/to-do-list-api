eef24f49b0f74c6a71b5f68bcc37dece0fcac5ff
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-09-06 23:06:53 +0500

    add get_to_do_items and pagination

--- src/to-do-api/db/connection.py
+++ src/to-do-api/db/connection.py
@@ -39,7 +39,7 @@ def create_user(username: str, email: str, password: str) -> str | None:
 
     cursor = connection.execute(
         queries.find_username_or_email,
-        (username, email)
+          (username, email)
     )
     exists = cursor.fetchone()
     if exists:
@@ -68,7 +68,7 @@ def create_user(username: str, email: str, password: str) -> str | None:
 
 
 def login(email: str, password: str) -> str | None:
-    """
+    """ 
     Return:
         None: invalid login information
         token(str): valid login information
@@ -109,7 +109,7 @@ def _verify_token(token: str) -> dict | None:
     """
     try:
         payload = jwt.decode(
-            token,
+            token, 
             SECRET_KEY,
             algorithms=["HS256"]
         )
@@ -158,11 +158,7 @@ def create_to_do_item(token: str, title: str, desc: str) -> dict | None:
     return response
 
 
-def update_to_do_item(
-    token: str,
-    todo_id: int,
-    title: str, desc: str
-) -> dict | None:
+def update_to_do_item(token: str, todo_id: int, title: str, desc: str) -> dict | None:
     """
     Return:
         None: unauthenticated, unauthorized, or todo item doesn't exist
@@ -182,7 +178,7 @@ def update_to_do_item(
     create_tables(connection)
 
     cursor = connection.execute(
-        queries.update_todo_item,
+        queries.update_todo_item, 
         (title, desc, todo_id, user_id)
     )
     connection.commit()
@@ -194,7 +190,7 @@ def update_to_do_item(
 
     cursor = connection.execute(
         queries.get_updated_todo_item_index,
-        (user_id, todo_id)
+        (user_id, todo_id)    
     )
     index = cursor.fetchone()[0]
 
@@ -217,7 +213,7 @@ def delete_todo_item(token: str, todo_id: int) -> int | None:
         status_code(int): successful deletion
     """
 
-    payload = _verify_token(token)
+    payload = _verify_token(token) 
     if payload is None:
         return None
 
@@ -261,9 +257,6 @@ def get_todo_items(token: str, page: int, limit: int) -> dict | None:
     if user_id is None:
         return None
 
-    if page < 1 or limit < 1:
-        return None
-
     connection = sqlite3.connect("todo.db")
 
     create_tables(connection)
@@ -277,14 +270,13 @@ def get_todo_items(token: str, page: int, limit: int) -> dict | None:
 
     cursor = connection.execute(
         queries.get_total_todos_for_user_id,
-        (user_id,)
+        (user_id, )
     )
-    total_todos = cursor.fetchone()[0]
 
     connection.close()
 
     data = []
-    for index, (todo_id, title, desc) in enumerate(rows, start=offset + 1):
+    for index, (todo_id, title, desc) in enumerate(rows, start=1):
         curr_data = {
             "index": index,
             "id": todo_id,
@@ -297,7 +289,7 @@ def get_todo_items(token: str, page: int, limit: int) -> dict | None:
         "data": data,
         "page": page,
         "limit": limit,
-        "total": total_todos
+        "total": index
     }
 
     return response
